82d15410aeb358384a9888f76ffcf2d12d247ef2
Dovydas Čeponis <d.ceponis@adform.com>
2022-09-29 18:11:48 +0300

    Changed data types

--- BankWebAPI.ClassLibrary/Entities/Account.cs
+++ BankWebAPI.ClassLibrary/Entities/Account.cs
@@ -9,9 +9,9 @@ namespace BankWebAPI.ClassLibrary.Entities
     public class Account
     {
         public int Id { get; set; }
-        public int AccountNumber { get; set; }
+        public string AccountNumber { get; set; }
         public string AccountType { get; set; }
-        public int Balance { get; set; }
+        public double Balance { get; set; }
         public int ClientId { get; set; }
     }
 }
